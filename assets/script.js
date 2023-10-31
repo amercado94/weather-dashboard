c0606f83d3c16de999427cc38ec99be6f482080c
Alfredo <alfredomercado1994@gmail.com>
2023-10-30 23:44:24 -0500

    launching a link change

--- assets/script.js
+++ assets/script.js
@@ -69,7 +69,7 @@ const getCityCoordinates = () => {
     const cityName = cityInput.value.trim(); // Get user entered city name and remove extra spaces
     if(!cityName) return; // Return if cityName is empty
     // console.log(cityName);
-    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
 
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
         if(!data.length) return alert(`No coordinates found for ${cityName}`);
